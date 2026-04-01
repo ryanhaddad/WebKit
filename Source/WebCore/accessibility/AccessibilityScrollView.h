68013d7f4ebbc58f14cf0df375e87c9f3b4a496b
Ryosuke Niwa <rniwa@webkit.org>
2026-03-31 17:05:02 -0700

    Delete incorrect NODELETE annotation from AccessibilityScrollView::addLocalFrameChild https://bugs.webkit.org/show_bug.cgi?id=311183
    Reviewed by Geoffrey Garen and Anne van Kesteren.
    
    Remove NODELETE annotation from AccessibilityScrollView::addLocalFrameChild since
    the annotation is wrong in iOS where ENABLE(ACCESSIBILITY_LOCAL_FRAME) is enabled.
    
    No new tests since there should be no behavioral changes.
    
    * Source/WebCore/accessibility/AccessibilityScrollView.h:
    
    Canonical link: https://commits.webkit.org/310345@main

--- Source/WebCore/accessibility/AccessibilityScrollView.h
+++ Source/WebCore/accessibility/AccessibilityScrollView.h
@@ -107,7 +107,7 @@ private:
     void setFocused(bool) final;
     bool canSetFocusAttribute() const final;
     bool isFocused() const final;
-    void NODELETE addLocalFrameChild();
+    void addLocalFrameChild();
     void addRemoteFrameChild();
     const AccessibilityScrollView* frameRootScrollView() const;
 
