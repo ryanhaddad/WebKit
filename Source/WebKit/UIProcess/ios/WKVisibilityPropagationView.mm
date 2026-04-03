ryanhaddad/WebKit

commit fb284a4c47a75cbbbf8ca10290a139df8aba719b

macCatalyst build failure in WKVisibilityPropagationView https://bugs.webkit.org/show_bug.cgi?id=311375 rdar://173972460
Unreviewed build fix.

* Source/WebKit/UIProcess/ios/WKVisibilityPropagationView.mm:

Canonical link: https://commits.webkit.org/310479@main

Source/WebKit/UIProcess/ios/WKVisibilityPropagationView.mm
@@ -34,6 +34,10 @@
 #import <wtf/RetainPtr.h>
 #import <wtf/WeakPtr.h>
 
+#if PLATFORM(IOS_FAMILY)
+#import <pal/spi/ios/UIKitSPI.h>
+#endif
+
 namespace WebKit {
 #if USE(EXTENSIONKIT)
 using ProcessAndVisibilityPropagatorPair = std::pair<WeakPtr<AuxiliaryProcessProxy>, RetainPtr<id<UIInteraction>>>;

commit 1db6dbd632e8992971b062a65026a6179da3e6d9

[iOS] Open source build broken after 310479@main https://bugs.webkit.org/show_bug.cgi?id=311404 rdar://174002545
Unreviewed build fix.

310479@main pulls in PAL's UIKitSPI header into WebKit. This causes a
problem for (some) unified source which happens to include WebKit's
UIKitSPI header too. In the open source build, we declare a lot of SPI
interfaces, and these declarations are now duplicated, causing build
errors.

In this patch, we address the issue by simply including WebKit's version
of UIKitSPI.h instead.

* Source/WebKit/UIProcess/ios/WKVisibilityPropagationView.mm:

Canonical link: https://commits.webkit.org/310509@main

Source/WebKit/UIProcess/ios/WKVisibilityPropagationView.mm
@@ -35,7 +35,7 @@
 #import <wtf/WeakPtr.h>
 
 #if PLATFORM(IOS_FAMILY)
-#import <pal/spi/ios/UIKitSPI.h>
+#import "UIKitSPI.h"
 #endif
 
 namespace WebKit {